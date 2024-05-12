8cd465aec9c1d2ffc83b20c33113294c299ff287
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-05-12 22:09:19 +0300

    Homework 3: Update balancer statistics page

--- Homework 3/frontend/src/pages/balancerStatistics/logic.ts
+++ Homework 3/frontend/src/pages/balancerStatistics/logic.ts
@@ -1,12 +1,15 @@
 import { useCallback, useEffect, useState } from "react"
 import { useSelector } from "react-redux"
-import { fetchPeers, getPeers, getStatistics, startFetchingStatistics, stopFetchingStatistics } from "../../reducer/balancers/reducer"
+import { eraseRootLoadBalancer, fetchPeers, getPeers, getRootBalancer, getStatistics, startFetchingStatistics, stopFetchingStatistics } from "../../reducer/balancers/reducer"
 import { useAppDispatch } from "../../reducer/store"
 import { unwrapResult } from "@reduxjs/toolkit"
 import { addNotification } from "../../reducer/notifications/reducer"
+import { useNavigate } from "react-router-dom"
 
 export const useLoadBalancerStatistics = () => {
+    const navigate = useNavigate()
     const dispatch = useAppDispatch()
+    const rootBalancer = useSelector(getRootBalancer)
     const peers = useSelector(getPeers)
     const statistics = useSelector(getStatistics)
     const [isPageReady, setIsPageReady] = useState(false)
@@ -34,9 +37,17 @@ export const useLoadBalancerStatistics = () => {
         }
     }, [performPageInit, dispatch])
 
+    const onClickBack = () => {
+        dispatch(eraseRootLoadBalancer())
+        navigate('/app/selection')
+    }
+
     return {
         isPageReady,
+        rootBalancer,
         peers,
-        statistics
+        statistics,
+
+        onClickBack
     }
 }
