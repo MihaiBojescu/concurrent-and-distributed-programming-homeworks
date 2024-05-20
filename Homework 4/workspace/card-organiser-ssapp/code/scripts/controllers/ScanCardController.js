b959ec4545d30379eade0857eaee8878a3c04db6
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-05-21 01:58:36 +0300

    Homework 4: Use zbar instead of html5-qrcode for scanning

--- Homework 4/workspace/card-organiser-ssapp/code/scripts/controllers/ScanCardController.js
+++ Homework 4/workspace/card-organiser-ssapp/code/scripts/controllers/ScanCardController.js
@@ -4,8 +4,12 @@ const { navigateToPageTag } = WebCardinal.preload;
 const { Controller } = WebCardinal.controllers;
 
 export default class ScanCardController extends Controller {
+    #done = false
+    #stream = void 0
+    #video = void 0
+    #canvas = void 0
+    #context = void 0
     #cardsRepository = void 0
-    #qrScanner = void 0
 
     constructor(...props) {
         super(...props);
@@ -16,7 +20,11 @@ export default class ScanCardController extends Controller {
             serial: '',
         }
 
-        this.#qrScanner = new Html5Qrcode("reader")
+        this.#done = false
+        this.#stream = navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        this.#video = document.querySelector('.page-cover video')
+        this.#canvas = new OffscreenCanvas(window.screen.availWidth, window.screen.availHeight)
+        this.#context = this.#canvas.getContext('2d')
         this.#cardsRepository = cardsRepositoryInstance.get()
         this.#cardsRepository
             .then(() => document.dispatchEvent(new CustomEvent('add-card-controller-loaded', { detail: { error: null } })))
@@ -29,52 +37,75 @@ export default class ScanCardController extends Controller {
 
     async #onInit() {
         try {
-            const devices = await Html5Qrcode.getCameras()
-            const cameraId = devices[0]?.id
-
             this.#cardsRepository = await this.#cardsRepository
-            this.#qrScanner.start(
-                cameraId,
-                { fps: 10 },
-                this.#onScanSuccess.bind(this),
-                this.#onScanFailed.bind(this)
-            )
+            this.#stream = await this.#stream
+            this.#video.srcObject = this.#stream
+
+            window.requestAnimationFrame(this.#videoLoop.bind(this))
         } catch (error) {
             console.error(error)
         }
     }
 
-    async #onScanSuccess(decodedText, decodedResult) {
+    async #videoLoop() {
+        await this.#processVideo()
+
+        if (this.#done) {
+            return
+        }
+
+        window.requestAnimationFrame(this.#videoLoop.bind(this))
+    }
+
+    async #processVideo() {
+        this.#canvas.width = this.#video.naturalWidth || this.#video.videoWidth || this.#video.width
+        this.#canvas.height = this.#video.naturalHeight || this.#video.videoHeight || this.#video.height
+
+        if (!this.#canvas.width || !this.#canvas.height) {
+            return
+        }
+
+        this.#context.drawImage(this.#video, 0, 0)
+
+        const imageData = this.#context.getImageData(0, 0, this.#canvas.width, this.#canvas.height)
+        const symbols = await zbarWasm.scanImageData(imageData)
+        const decodedSymbols = symbols.map(symbol => ({
+            ...symbol,
+            decoded: symbol.decode()
+        }))
+
+        if (decodedSymbols.length === 0) {
+            return
+        }
+
+        await this.#videoCleanups()
+        await this.#onScanSuccess(decodedSymbols[0].decoded, decodedSymbols[0].typeName)
+    }
+
+    async #videoCleanups() {
+        this.#stream.getTracks().forEach(track => track.stop())
+        this.#done = true
+    }
+
+    async #onScanSuccess(decodedText, decodedFormat) {
         this.model.serial = decodedText
-        this.model.type = decodedResult.result.format.formatName
+        this.model.type = decodedFormat
 
         try {
-            await this.#cardsRepository.addCard(
+            const card = await this.#cardsRepository.addCard(
                 this.model.brand,
                 '',
                 this.model.type,
                 this.model.serial
             )
+            console.log({ card })
         } finally {
             navigateToPageTag('home')
         }
     }
 
-    #onScanFailed(error) {
-    }
-
     #onGoBack(model, target, event) {
         event.stopImmediatePropagation()
         navigateToPageTag('select-card-brand')
-    }
-
-    async onDisconnectedCallback() {
-        try {
-            await this.#qrScanner.stop()
-        } catch { }
-
-        try {
-            await this.#qrScanner.clear()
-        } catch { }
     }
 }
